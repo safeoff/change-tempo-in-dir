684cbc4b1df22b51094c7d95b4e24c8de8c3f450
ry <sgmrrc@gmail.com>
2018-04-17 23:03:38 +0900

    lsを--ignoreから*[!word]に変更

--- change-tempo-in-dir.sh
+++ change-tempo-in-dir.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 mkdir -p files
-for a in `ls --ignore=*.sh -F | grep -v /`; do
-    ffmpeg -y -i $a -vf setpts=PTS/$1 -af atempo=$1 files/$a
+for a in `ls *[!.sh] -F | grep -v /`; do
+    ffmpeg -y -i $a -vf setpts=PTS/$1 -af atempo=$1 files/$a -loglevel warning
     echo $a
 done
